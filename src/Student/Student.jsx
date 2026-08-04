331468136b9f6e760aaa9f2df9b75c99e33c6714
AnnamVenkataNagendra <annamvenkatanagendra@gmail.com>
2026-08-04 22:24:19 +0530

    Added React login with JWT authentication

--- src/Student/Student.jsx
+++ src/Student/Student.jsx
@@ -43,51 +43,47 @@ const navigate=useNavigate();
     }
 
   return (
- <form onSubmit={handelLogin}>
-  <div className="container border border-3 w-50 p-3 mt-3 rounded-3">
+  <>
+    <form onSubmit={handelLogin}>
+      <div className="container border border-3 w-50 p-3 mt-3 rounded-3">
 
-    <h1 className="bi bi-person-fill text-primary">
-      User Login Form
-    </h1>
+        <h1>User Login Form</h1>
 
-    <div className="mb-3">
-      <label>User Name</label>
-      <input
-    type="text"
-    className="form-control"
-    value={getName.stuName}
-    onChange={(e) =>
-        setName({ ...getName, stuName: e.target.value })
-    }
-/>
+        <div className="mb-3">
+          <label>User Name</label>
+          <input
+            type="text"
+            className="form-control"
+            value={getName.stuName}
+            onChange={(e) =>
+              setName({ ...getName, stuName: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            value={getName.stuPass}
+            onChange={(e) =>
+              setName({ ...getName, stuPass: e.target.value })
+            }
+          />
+        </div>
+
+        <button type="submit" className="btn btn-danger">
+          Login
+        </button>
+      </div>
+    </form>
+
+    <div className="text-center mt-3">
+      <span>New User? </span>
+      <Link to="/post">Create Account</Link>
     </div>
-
-    <div className="mb-3">
-      <label>Password</label>
-      <input
-    type="password"
-    className="form-control"
-    value={getName.stuPass}
-    onChange={(e) =>
-        setName({ ...getName, stuPass: e.target.value })
-    }
-/>
-    </div>
-
-    <button type="submit" className="btn btn-danger">
-      Login
-    </button>
-
-    <div className="mt-3">
-      <span>New User?</span>
-
-      <Link to="/post" className="btn btn-primary ms-2">
-        Create Account
-      </Link>
-    </div>
-
-  </div>
-</form>
+  </>
   )
 }
 
